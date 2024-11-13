cd4d5c36208e5ae2ba27273e2e174561b3501233
Onkar Nanavare <onkarnanvare5554@gmail.com>
2024-11-13 14:12:27 +0530

    add styling

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <>
-      <div className="h-screen bg-red-800 w-full">
+      <div className="h-screen bg-pink-300 w-full">
         <h1>skills</h1>
       </div>
     </>
