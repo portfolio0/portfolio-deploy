9dcc7fce6d6e5192b840f8986bb61c238d4570d8
Onkar Nanavare <onkarnanvare5554@gmail.com>
2025-07-01 23:24:24 +0530

    improvement 1-7-25

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import nav_logo from "../../assets/nav_logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -8,9 +8,33 @@ import "./navbar.css";
 const Navbar = () => {
   const [isopen, setIsOpen] = useState(true);
 
+  const [shownavbar, setshownavbar] = useState(true);
+
   const handlenavclick = () => {
     setIsOpen(true);
   };
+
+  useEffect(() => {
+    let previousscrolly = window.scrollY;
+
+    const handlescroll = () => {
+      const currentscrollyposition = window.scrollY;
+      if (
+        currentscrollyposition > previousscrolly &&
+        currentscrollyposition > 50
+      ) {
+        setshownavbar(false);
+      } else {
+        setshownavbar(true);
+      }
+      previousscrolly = currentscrollyposition;
+    };
+
+    window.addEventListener("scroll", handlescroll);
+    return () => {
+      window.removeEventListener("scroll", handlescroll);
+    };
+  }, []);
 
   return (
     // <div
@@ -19,7 +43,8 @@ const Navbar = () => {
     //   }`}
     // >
     <div
-      className={`nav w-full backdrop-blur-md bg-white/50 top-0 z-50 transition-all duration-300 ${
+      className={`nav w-full backdrop-blur-md bg-white/50 top-0 z-50 transition-all duration-300
+        ${shownavbar ? "translate-y-0" : "-translate-y-full"} ${
         isopen
           ? "flex items-center justify-between px-5 p-3 sticky"
           : "fixed h-screen flex flex-col items-center justify-start pt-20 px-5"
